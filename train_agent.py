de08e323c909e7ffdb8b6211b5e6ba891c924476
Андрей Кустов <andrey_kustov@192.168.1.5>
2022-11-11 01:20:24 +0300

    add DialogFlow learning script

---
+++ train_agent.py
@@ -0,0 +1,42 @@
+import json
+
+from environs import Env
+from google.cloud import dialogflow
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+
+    with open('training_dataset.json', 'r') as file:
+        training_dataset = file.read()
+
+    project_id = env.str('PROJECT_ID')
+    training_datasets = json.loads(training_dataset)
+
+    for display_name, dataset in training_datasets.items():
+        create_intent(project_id, display_name, dataset['questions'], [dataset['answer']])

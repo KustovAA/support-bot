3c48a2bd9e1efd7857876b130bd19319eb643753
Андрей Кустов <andrey_kustov@192.168.1.5>
2022-11-11 14:58:56 +0300

    add DialogFlow to VK bot

--- vk.py
+++ vk.py
@@ -1,8 +1,9 @@
 import logging
+import random
 
 from environs import Env
 from google.cloud import dialogflow
-import vk_api
+import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
@@ -21,6 +22,23 @@ def send_message_to_dialog_flow(session_id, project_id, text, language_code = 'r
     return response.query_result.fulfillment_text
 
 
+def dialog(session_id, project_id, event, vk_api):
+    dialog_flow_response = send_message_to_dialog_flow(session_id, project_id, event.text)
+
+    if dialog_flow_response:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=dialog_flow_response,
+            random_id=random.randint(1, 1000)
+        )
+    else:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message='Не совсем понимаю, о чем ты.',
+            random_id=random.randint(1, 1000)
+        )
+
+
 if __name__ == '__main__':
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -30,16 +48,13 @@ if __name__ == '__main__':
     env = Env()
     env.read_env()
     token = env.str('VK_ACCESS_TOKEN')
+    session_id = env.str('SESSION_ID')
+    project_id = env.str('PROJECT_ID')
 
-    vk_session = vk_api.VkApi(token=token)
-
+    vk_session = vk.VkApi(token=token)
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            dialog(session_id, project_id, event, vk_api)
